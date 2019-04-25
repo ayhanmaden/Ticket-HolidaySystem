6f34850fcbe828041e4cc4165af5f49e18751fc4
Meryem <Ezber>
2019-04-25 18:06:04 +0300

    edit some

--- seyehat/src/seyehat/biletOtelRezervasyon.java
+++ seyehat/src/seyehat/biletOtelRezervasyon.java
@@ -9,12 +9,12 @@ package seyehat;
  *
  * @author meryemezber
  */
-public class NewJFrame1 extends javax.swing.JFrame {
+public class biletOtelRezervasyon extends javax.swing.JFrame {
 
     /**
      * Creates new form NewJFrame1
      */
-    public NewJFrame1() {
+    public biletOtelRezervasyon() {
         initComponents();
     }
 
@@ -555,20 +555,21 @@ public class NewJFrame1 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(biletOtelRezervasyon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(biletOtelRezervasyon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(biletOtelRezervasyon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(biletOtelRezervasyon.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new NewJFrame1().setVisible(true);
+                new biletOtelRezervasyon().setVisible(true);
             }
         });
     }
